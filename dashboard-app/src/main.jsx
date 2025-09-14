dbc016ba267ab2e0cb1489fff6761c57a8ad8e28
patilpradnesh <patilpradnesh4124@gmail.com>
2025-09-14 12:55:58 +0530

    deployment resolve

--- dashboard-app/src/main.jsx
+++ dashboard-app/src/main.jsx
@@ -6,7 +6,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import store from './redux/Store.js';
+import store from './redux/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
